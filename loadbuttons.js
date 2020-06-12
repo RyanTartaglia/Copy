05db0292e3e4677107bac9a0a61571de0809d941
Ryan <ryan@tartaglia.biz>
2020-06-12 14:49:19 -0400

    home page, unicode

--- loadbuttons.js
+++ loadbuttons.js
@@ -1,6 +1,18 @@
-function setButtons(){
-   var elements = document.getElementsByClassName("cb");
-   for(var i = 0; i < elements.length; i++){
-      elements[i].onclick = function(){};
-   }
+function copy(that){
+   var inp =document.createElement('input');
+   document.body.appendChild(inp);
+   inp.value =that.textContent;
+   inp.select();
+   document.execCommand('copy',false);
+   inp.remove();
 }
+
+window.onload = function() {
+    var anchors = document.getElementsByClassName("cb");
+    for(var i = 0; i < anchors.length; i++) {
+        var anchor = anchors[i];
+        anchor.onclick = function() {
+            copy(this);
+        };
+    }
+};
